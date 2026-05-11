4d945064345c7c6188594b481a4977ac5b930988
Heisenberg1912 <batham.tushar2001@gmail.com>
2026-05-11 14:59:15 +0530

    fix: remove sidebar and bottom nav from landing page layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,28 +1,11 @@
-import { ReportModalProvider } from '@/components/report/ReportModalProvider';
-import { Sidebar } from '@/components/nav/Sidebar';
-import { BottomNav } from '@/components/nav/BottomNav';
-
 export default function PublicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ReportModalProvider>
-      <div className="relative min-h-screen lg:flex">
-        {/* Sidebar for Desktop */}
-        <div className="hidden lg:block">
-          <Sidebar />
-        </div>
-
-        {/* Main Content */}
-        <div className="min-w-0 flex-1 lg:pl-64">
-          {children}
-        </div>
-
-        {/* Bottom Nav for Mobile */}
-        <BottomNav />
-      </div>
-    </ReportModalProvider>
+    <div className="relative min-h-screen">
+      {children}
+    </div>
   );
 }
